d08dea3e6d13c80dbebbfa564ef4f96a3021179b
Dariusz Podczasik <boostertech@mail.com>
2024-02-03 16:30:52 +0000

    Navigation component created & styled

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import { Link, Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage/index";
 import TaskPage from "./features/tasks/TaskPage/index";
 
 import AuthorPage from "./features/author/index";
+import { Navigation } from "./common/TopNavigation/Navigation";
 
 const App = () => (
   <HashRouter basename="/todo-list-module-14">
-    <nav>
-      <ul>
-        <li>
-          <Link to="/tasks">Tasks</Link>
-        </li>
-        <li>
-          <Link to="/author">About Author</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/tasks/:id">
-          <TaskPage />
-        </Route>
-        <Route path="/tasks">
-          <TasksPage />
-        </Route>
-        <Route path="/author">
-          <AuthorPage />
-        </Route>
-        <Route path="/">
-          <Redirect to="/tasks" />
-        </Route>
-      </Switch>
-    </nav>
+    <Navigation />
+    <Switch>
+      <Route path="/tasks/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/tasks">
+        <TasksPage />
+      </Route>
+      <Route path="/author">
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to="/tasks" />
+      </Route>
+    </Switch>
   </HashRouter>
 );
 
