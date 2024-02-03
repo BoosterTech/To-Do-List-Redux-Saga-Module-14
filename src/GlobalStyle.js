a639cc66235c5931988643e61016c82cd14b1868
Dariusz Podczasik <boostertech@mail.com>
2024-02-03 22:25:10 +0000

    Global style updated

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -17,7 +17,7 @@ body {
   margin:0;
   padding-right: 15px;
   font-size: small;
-  overscroll-behavior-y:auto;
+  overflow-x: hidden;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
     overscroll-behavior-y:auto;
