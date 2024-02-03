90be78264a79072d34182c31435b2a4a72959093
Dariusz Podczasik <boostertech@mail.com>
2024-02-03 18:49:19 +0000

    Updates

--- src/features/tasks/taskslocalStorage.js
+++
@@ -1,9 +0,0 @@
-const localStorageKey = "tasksData";
-
-export const saveTasksToLocalStorage = (tasks) => {
-  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
-};
-
-export const loadTasksFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem(localStorageKey)) || [];
-
--- src/react-router-dom.min.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'react-router-dom/cjs/react-router-dom.min';
