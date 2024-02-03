de01eb8f981625b79a02496c485e3bdf9fbec0df
Dariusz Podczasik <boostertech@mail.com>
2024-02-03 14:09:49 +0000

    exampleTasks link changed

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,6 +1,6 @@
 export const getExampleTasks = async () => {
   const response = await fetch(
-    "http://localhost:3000/to-do-list-redux-saga/exampleTasks.json"
+    "To-Do-List-Redux-Saga-Module-14/exampleTasks.json"
   );
 
   if (!response.ok) {
